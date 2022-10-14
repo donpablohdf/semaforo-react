6f37df5766ddfba049150fc9d188cc6db8ce796a
Don Pablo el de Felicitas <pabloalegrejuan@gmail.com>
2022-10-14 05:28:12 +0000

    Con algun fallo

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
+const enRoja= "luzRoja"
+const enAmarilla="luzAmarilla"
+const enVerde="luzVerde"
+const enMagenta="luzMagenta"
+let IDintervalo;
 
+const enNegro="luzNegra"
 
 //create your first component
 const TrafficLight = () => {
-	const enRoja= "luzRoja"
-	const enAmarilla="luzAmarilla"
-	const enVerde="luzVerde"
-	const enMagenta="luzMagenta"
 
-	const enNegro="luzNegra"
-
+//declaramos las variables
 	const [Roja, setRoja] = useState(enNegro);
 	const [Amarilla, setAmarilla] = useState(enNegro);
 	const [Verde, setVerde] = useState(enNegro);
@@ -18,23 +19,10 @@ const TrafficLight = () => {
 	const [creaMG, setMG]= useState(false);
 	//control de semaforo
 	const [ParaSemaforo, setPara]= useState(false);
-	const [IDinterval, setIter] = useState(0);
-
-	const arrancaSemaforo = () =>{
-		
-		if(!ParaSemaforo && !creaMG){ tresColores();} 
-		if(!ParaSemaforo && creaMG) { cuatroColores();}
-		setPara(true);
-	}
-
-	const paraSemaforo= () =>{		
-		setPara(false);
-		setIter(clearInterval(IDinterval));
-	}
-
+	
+// movimiento de luces
 	const tresColores = () =>{
-		setRoja(enRoja);
-		setIter (setInterval(() => {
+		IDintervalo = setInterval(() => {
 			setRoja(enRoja);
 			setAmarilla(enNegro);
 			setVerde(enNegro);
@@ -43,76 +31,161 @@ const TrafficLight = () => {
 				setRoja(enNegro);
 				setAmarilla(enAmarilla);
 				setVerde(enNegro);
-			}, 1000);	
+			}, 333);	
 			setTimeout(()=>{
 				setRoja(enNegro);
 				setAmarilla(enNegro);
 				setVerde(enVerde);
-			}, 1500);
-		}, 2000));
+			}, 666);
+		}, 999);
+		setTimeout(() => {
+			setRoja(enRoja);
+		}, 1005);
+		
+		
+
 	}
 	const cuatroColores = () =>{
-		setMagenta(enMagenta);
-		setIter (setInterval(() => {
-			setMagenta(enMagenta);
-			setRoja(enNegro);
+		
+		IDintervalo = setInterval(() => {
+			setRoja(enRoja);
 			setAmarilla(enNegro);
 			setVerde(enNegro);
-
-			setTimeout(()=>{
-				setMagenta(enNegro);
-				setRoja(enNegro);
-				setAmarilla(enNegro);
-				setVerde(enVerde);
-			}, 1000);
+			setMagenta(enNegro);
 			
 			setTimeout(()=>{
-				setMagenta(enNegro);
 				setRoja(enNegro);
 				setAmarilla(enAmarilla);
 				setVerde(enNegro);
-			}, 1500);	
-
-			setTimeout(()=>{
 				setMagenta(enNegro);
-				setRoja(enRoja);
+			}, 333);	
+			setTimeout(()=>{
+				setRoja(enNegro);
+				setAmarilla(enNegro);
+				setVerde(enVerde);
+				setMagenta(enNegro);
+			}, 666);
+			setTimeout(()=>{
+				setRoja(enNegro);
 				setAmarilla(enNegro);
 				setVerde(enNegro);
-			}, 2000);
-		}, 2500));
+				setMagenta(enMagenta);
+			}, 999);
+		}, 1332);
+		setTimeout(() => {
+			setRoja(enRoja);
+		}, 1005);
 	}
-	return (
-	<><div>
-			<div className="p-3 bg-warning rounded-top"><i className={"fas fa-circle fa-5x " + Roja} onClick={() => Roja == "luzRoja" ? setRoja(enNegro) : setRoja(enRoja)}></i></div>
-			<div className="p-3 bg-warning"><i className={"fas fa-circle fa-5x " + Amarilla} onClick={() => Amarilla == "luzAmarilla" ? setAmarilla(enNegro) : setAmarilla(enAmarilla)}></i></div>
-			<div className="p-3 bg-warning shadow"><i className={"fas fa-circle fa-5x " + Verde} onClick={() => Verde == "luzVerde" ? setVerde(enNegro) : setVerde(enVerde)}></i></div>
-			{creaMG ? (<div className="p-3 bg-warning shadow"><i className={"fas fa-circle fa-5x " + Magenta} onClick={() => Magenta == "luzMagenta" ? setMagenta(enNegro) : setMagenta(enMagenta)}></i></div>) : (<></>)}
+	const todosNegro= () =>{
+		setMagenta(enNegro);
+		setRoja(enNegro);
+		setAmarilla(enNegro);
+		setVerde(enNegro);
+	}
 
-			<div className="row shadow p-1 bg-light mt-2 ">
-				<div className='col p-3'>
-					{ParaSemaforo ? (<i className="fas fa-stop fa-2x " onClick={() => paraSemaforo()}></i>) : (<i className="fas fa-play fa-2x " onClick={() => arrancaSemaforo()}></i>)}
-				</div>
-				<div className='col p-3'>
-					{creaMG ? (<i className="fas fa-minus fa-2x" onClick={() => setMG(false)}></i>) : (<i className="fas fa-plus fa-2x" onClick={() => setMG(true)}></i>)}
+//control del semaforo
+	const arrancaSemaforo = () =>{
+		
+		if(!ParaSemaforo && !creaMG){ setTimeout(() => {
+			tresColores();			
+		}, 50); }
+
+		if(!ParaSemaforo && creaMG) { setTimeout(() => {
+			cuatroColores();			
+		}, 50); }
+		setPara(true);
+		todosNegro();
+	}
+	
+
+	const pararSemaforo= () =>{		
+		
+		setPara(false);
+		clearInterval(IDintervalo);
+		todosNegro();
+		
+	}
+// ************************** J S X ***************************	
+	return (
+<>
+{/* GRUPO SEMÁFORO Y CAJA DE CONTROL */}
+	<div>
+		{/* CAJA DE CONTROL DE SEMÁFORO Y CREACIÓN DE MAGENTA*/}
+			<div>
+				
+				<div className="row  p-1  mb-2 ">
+					{/* para o arranca el semáforo*/}
+					<div className='col p-3 bg-light d-flex justify-content-start'>
+						{ParaSemaforo ? (<i 
+											className="fas fa-stop fa-2x " onClick={() =>{ pararSemaforo(); setTimeout(() => {todosNegro();}, 200);}}></i>) : (<i className="fas fa-play fa-2x " onClick={() => arrancaSemaforo()}></i>)}
+					</div>
+					{/* botón crea y mata magenta */}
+					<div className='col p-3 bg-light d-flex justify-content-end'>					
+						{creaMG ? (<i className="fas fa-minus fa-2x" onClick={() =>{ setMG(false); pararSemaforo();}}></i>) : (<i className="fas fa-plus fa-2x" onClick={() =>{ setMG(true); paraSemaforo();}}></i>)}
+					</div>
 				</div>
 			</div>
-		</div>
-		<div className='m-5'>
-			<ul className="list-group list-group-flush">
-				<li className="list-group-item">Al pulsar sobre cualquier círculo cambia de negro a su color correspondiente y viceversa</li>
-				<li className="list-group-item">Al pulsar sobre <i className="fas fa-plus"></i> crea el círculo magenta, que al pulsar sobre él cambia el color a negro y viceversa.<br/> 
-				Al pulsar sobre <i className="fas fa-minus"></i> desaparece el círculo magenta</li>
-				<li className="list-group-item">Al pulsar sobre <i className="fas fa-play"></i> arranca el semáforo y cambia el estado del botón a <i className="fas fa-stop"></i> para poder pararlo.<br/>
-				Funciona tanto como si está el magenta como si no.  </li>
-				<li className="list-group-item bg-danger text-center h2">DEPURAR</li>
-				<li className="list-group-item bg-danger ">El semáforo no funciona como debería. Creo que por settimeout</li>
-				<li className="list-group-item bg-danger ">Parar el semáforo no funciona como debería.<br/> He tenido que usar una variable useState para meter el intervalo porque así es como mejor se para. </li>
-				<li className="list-group-item bg-danger ">El borde redondeado de arriba no coge el diámetro que le mando.<br/>Si le pongo rounded-3 redondea las cuatro esquinas.</li>
-				<li className="list-group-item bg-danger ">¿Las variables useState como se pasan de un componete a otro?</li>
-				<li className="list-group-item bg-danger ">No tengo ni idea de para que sirve useEffect o si tenía que usarlo en ésta práctica.</li>
-			</ul>
-		</div>
-		</>
+
+		{/* SEMAFORO */}
+			<div className='bg-success p-3 rounded-pill '>
+				<div>
+					{/* luces roja, amarilla y verde */}
+					<div className="row p-3 bg-warning rounded-circle m-2 border border-dark border-2">
+						<i 
+							className={"fas fa-circle fa-5x rounded-circle border border-success border-3 " + Roja} 
+							onClick={() => Roja == "luzRoja" ? setRoja(enNegro) : setRoja(enRoja)}
+						></i>
+					</div>
+					<div className="row p-3 bg-warning rounded-circle m-2 border border-dark border-2">
+						<i 
+							className={"fas fa-circle fa-5x rounded-circle border border-success border-3 " + Amarilla} 
+							onClick={() => Amarilla == "luzAmarilla" ? setAmarilla(enNegro) : setAmarilla(enAmarilla)}
+						></i>
+					</div>
+					<div className="row p-3 bg-warning rounded-circle m-2 border border-dark border-2">
+						<i 
+							className={"fas fa-circle fa-5x rounded-circle border border-success border-3 " + Verde} 
+							onClick={() => Verde == "luzVerde" ? setVerde(enNegro) : setVerde(enVerde)}
+						></i>
+					</div>
+
+					{/* crea o mata magenta según el botón */}
+					{ //si pulsamos el botón de crear magenta crea el nuevo elemento 
+					creaMG ? ( 
+						<div className="row p-3 bg-warning rounded-circle m-2 border border-dark border-2">
+							<i 
+							className={"fas fa-circle fa-5x rounded-circle border border-success border-3 " + Magenta} 
+							onClick={
+								//se puede cambiar el color de magenta a negro y viceversa al pulsar sobre él
+								() => Magenta == "luzMagenta" ? setMagenta(enNegro) 
+								: setMagenta(enMagenta)
+							}>
+							</i>
+						</div>
+					) 
+					// si pulsamos el botón de eliminar magenta	crea un fragmento vacío		
+					: (<></>)}
+				</div>
+			</div>
+	</div>
+			
+{/* INSTRUCCIONES Y BUGS*/}
+	<div className='ms-5 border-danger border-3'>
+		<ul className="list-group list-group-flush">
+			<li className="list-group-item">Al pulsar sobre cualquier círculo cambia de negro a su color correspondiente y viceversa</li>
+			<li className="list-group-item">Al pulsar sobre <i className="fas fa-plus"></i> crea un nuevo círculo negro, que al pulsar sobre él cambia a magenta y viceversa.<br/> 
+			Al pulsar sobre <i className="fas fa-minus"></i> desaparece</li>
+			<li className="list-group-item">Al pulsar sobre <i className="fas fa-play"></i> arranca el semáforo y cambia el estado del botón a <i className="fas fa-stop"></i> para poder pararlo.<br/>
+			Funciona tanto como si está el magenta como si no.  </li>
+			<li className="list-group-item bg-danger text-center h2">DEPURAR</li>
+			<li className="list-group-item bg-danger ">Hay que parar el semáforo al pulsar sobre <i className="fas fa-plus"></i> </li>
+			<li className="list-group-item bg-danger ">No se elimina bien el intervalo al pulsar en <i className="fas fa-stop"></i><br/>He usado un settimeout en paraSemaforo</li>
+			
+			<li className="list-group-item bg-danger ">¿Las variables useState como se pasan de un componete a otro?</li>
+		</ul>
+	</div>
+
+</>
 	);
 };
 
